bb99739edf68d14983db26d4c00343e7c440de63
Reza Seyf <rseyf@hotmail.com>
2021-08-09 15:17:57 +0430

    init codebase

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'standard',
+    parserOptions: {
+        ecmaVersion: 9,
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['error', 4],
+        semi: ['error', 'always'],
+        camelcase: 0
+    }
+};
